30a3db6d12589c5f5db508b1ed23e0e9f558f790
jmarulandaerco <jmarulanda@erco.energy>
2025-02-13 16:42:09 -0500

    create - master charge information from .init

--- AppFrontend/Views/menuView.py
+++ AppFrontend/Views/menuView.py
@@ -17,80 +17,125 @@ list_config=[]
 
 class MeasureView(View):
     def get(self, request):
+        config.read(list_path_menu[0])
+
         sample_data = {
-            "zone": "UTC-5",
-            "modbus": "9600",
-            "start": "08:00",
-            "stop": "18:00",
+            "zone": config.get('measurementmodbus', 'timezone'),
+            "modbus": config.get('measurementmodbus'),
+            "start": config.get('measurementmodbus', 'start_hour'),
+            "stop": config.get('measurementmodbus', 'stop_hour'),
         }
         return JsonResponse(sample_data)
 
 class FormDataServer(View):
     def get(self, request):
         sample_data = {
-            "Server": "Neu plus",  # Puede ser "Telemetry", "Neu plus" o "All services"
-            "neu_plus": "123456",
-            "telemetry": "ABCDEF",
-            "mqtt": "1000",
-            "storage": "5000",
+            "Server": config.get('server', 'server_type'),  # Puede ser "Telemetry", "Neu plus" o "All services"
+            "neu_plus": config.get('server', 'id_device'),
+            "telemetry": config.get('server', 'identify_id'),
+            "mqtt": config.get('server', 'sampling_mqtt'),
+            "storage": config.get('server', 'sampling_storage_plus'),
         }
         return JsonResponse(sample_data)
     
 class FormDataModes(View):
     def get(self, request):
+        config.read(list_path_menu[0])
+        limitation='No'
+        if config.get('functioning', 'enable_active_limitation') == "False":
+            limitation='No'
+        else:
+             limitation='Yes'
+
+        compensation='No'
+
+        if config.get('functioning', 'enable_reactive_compensation') == "False":
+            compensation='No'
+        else:
+             compensation='Yes'
         sample_data = {
-             "mode": "Automatic",            # Valor de ejemplo para el modo
-            "limitation": "Yes",            # Opciones: "Yes" o "No"
-            "compensation": "Yes",           # Opciones: "Yes" o "No"
-            "sampling_limitation": "5s",    # Ejemplo: 5 segundos
-            "sampling_compensation": "10s", # Ejemplo: 10 segundos
+             "mode": config.get('functioning', 'work_mode'),            # Valor de ejemplo para el modo
+            "limitation": limitation,            # Opciones: "Yes" o "No"
+            "compensation": compensation,           # Opciones: "Yes" o "No"
+            "sampling_limitation": config.get('functioning', 'time_active_power'),    # Ejemplo: 5 segundos
+            "sampling_compensation": config.get('functioning', 'time_reactive_power'), # Ejemplo: 10 segundos
         }
         return JsonResponse(sample_data)
 
 class FormDataSettingDataBase(View):
     def get(self,request):
+        config.read(list_path_menu[0])
         sample_data = {
-            "day":"30",
-            "await":"2",
+            "day":config.get('database', 'old_days'),
+            "await":config.get('database', 'await_to_while'),
         }
         return JsonResponse(sample_data)
     
 class FormDataSettingInterface(View):
     def get(self,request):
         sample_data = {
-            "interface":"wlan0",
-            "connection":"Red-Onomondo",
+            "interface":config.get('internet_interfaces', 'internet_interface'),
+            "connection":config.get('internet_interfaces', 'connection_name'),
         }
         return JsonResponse(sample_data)
 
 class FormDataLimitation(View):
     def get(self,request):
+        config.read(list_path_menu[4])
+        limitation="No"
+        if config.getboolean('Active', 'energy_meter_3p', fallback=False)==True:
+            limitation="Yes"
+        else:
+            limitation="No" 
+
         sample_data= {
-            "limitation":"Yes",
-            "meter_ids":"11",
-            "inverter_ids":"1",
-            "porcentage":"0.02",
-            "grid_min":"5",
-            "grid_max":"10",
-            "inverter_min":"0",
-            "inverterMax":"1000",
+            "limitation":limitation,
+            "meter_ids":config.get('Active', 'energy_meter_ids', fallback="1,2"),
+            "inverter_ids":config.get('Active', 'inverter_ids', fallback="1,2"),
+            "porcentage":str(config.getfloat('Active', 'active_power_percentage', fallback=0.02)),
+            "grid_min":str(config.getfloat('Active', 'active_power_grid_min', fallback=5.0)),
+            "grid_max":str(config.getfloat('Active', 'active_power_grid_max', fallback=10.0)),
+            "inverter_min":str(config.getint('Active', 'active_power_inv_min', fallback=0)),
+            "inverterMax":str(config.getint('Active', 'active_power_inv_max', fallback=1000)),
 
         }
         return JsonResponse(sample_data)
 
 class FormDataCompensation(View):
-    def get(self,request):
-        sample_data= {
-            "reactive_power":"No",
-            "meter_ids":"4.5",
-            "smart_logger":"10",
-            "high":"0.4",
-            "low":"0.3",
-            "reactive":"0",
-            "active":"0",
-            "time":"0.1",
-            "factor":"0.91"
+    config.read(list_path_menu[5])
 
+    def get(self,request):
+        limitation="No"
+        if config.getboolean(
+                    "Reactive", "reactive_power_limiter", fallback=False
+                ) == True:
+            limitation="Yes"
+        else:
+            limitation="No"
+        sample_data= {
+            "reactive_power":limitation,
+            "meter_ids":config.get(
+                    "Reactive", "energy_meter_ids", fallback="4"
+                ),
+            "smart_logger":config.get(
+                    "Reactive", "smartlogger_id", fallback="10"
+                ),
+            "high":config.getfloat(
+                    "Reactive", "reactive_power_percentage_high", fallback=0.40
+                ),
+            "low":config.getfloat(
+                    "Reactive", "reactive_power_percentage_low", fallback=0.30
+                ),
+            "reactive":config.getint(
+                    "Reactive", "reactive_offset", fallback=0
+                ),
+            "active":config.getint(
+                    "Reactive", "active_offset", fallback=0
+                ),
+            "time":config.getfloat(
+                    "Reactive", "time_active_power", fallback=0.1
+                ),
+            "factor":config.getfloat("Reactive", "pf_min", fallback=0.91)
         }
         return JsonResponse(sample_data)
     
