93c9d5faae6a73e3501ffd4b37d8e015c34594e9
jmarulandaerco <josemarulanda63@hotmail.com>
2025-02-24 17:52:05 -0500

    edit - master bug fixed to generate json

--- AppFrontend/Views/jsonView.py
+++ AppFrontend/Views/jsonView.py
@@ -6,22 +6,18 @@ from rest_framework.views import APIView
 class ListColections(APIView):
     def get(self, request):
         try:
-            # Obtener la conexión a MongoDB desde Djongo
+
             db = connections['default']
 
-            # Obtener todas las colecciones
             colecciones = db.cursor().db_conn.list_collection_names()
 
-            # Obtener los datos de cada colección
             datos = {}
             for coleccion in colecciones:
                 collection_ref = db.cursor().db_conn[coleccion]
                 datos[coleccion] = list(collection_ref.find({}, {"_id": 0}))  # Excluir _id para evitar problemas de serialización
 
-            # Convertir datos a JSON
-            json_data = json.dumps({"colecciones": datos}, indent=4)
+            json_data = json.dumps({"colecciones": datos}, indent=4, default=str)
 
-            # Crear la respuesta con el archivo TXT
             response = HttpResponse(json_data, content_type="text/plain")
             response["Content-Disposition"] = 'attachment; filename="colecciones.txt"'
 
