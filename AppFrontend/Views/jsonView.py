61186210b8cb3f8f1707b996e3f9c5c3e2dde6b3
jmarulandaerco <josemarulanda63@hotmail.com>
2025-02-24 18:05:27 -0500

    edit - master edit the log function to not show user information

--- AppFrontend/Views/jsonView.py
+++ AppFrontend/Views/jsonView.py
@@ -4,27 +4,27 @@ from django.db import connections
 from rest_framework.views import APIView
 from datetime import datetime
 
-from django.http import HttpResponse
-from django.db import connections
-from rest_framework.views import APIView
-import json
-from datetime import datetime
-
-class ListCollections(APIView):
+class ListColections(APIView):
     def get(self, request):
         try:
+
             db = connections['default']
-            collections = db.cursor().db_conn.list_collection_names()
-            data = {}
-            for collection in collections:
-                if collection != 'authApp_user':
-                    collection_ref = db.cursor().db_conn[collection]
-                    data[collection] = list(collection_ref.find({}, {"_id": 0}))
-            json_data = json.dumps({"collections": data}, indent=4, default=str)
+
+            colecciones = db.cursor().db_conn.list_collection_names()
+
+            datos = {}
+            for coleccion in colecciones:
+                if coleccion != 'authApp_user':
+                    collection_ref = db.cursor().db_conn[coleccion]
+                    datos[coleccion] = list(collection_ref.find({}, {"_id": 0}))  # Excluir _id para evitar problemas de serialización
+
+            json_data = json.dumps({"colecciones": datos}, indent=4, default=str)
             current_datetime = datetime.now()
             filename = current_datetime.strftime('collections_%Y-%m-%d_%H-%M-%S.txt')
             response = HttpResponse(json_data, content_type="text/plain")
             response["Content-Disposition"] = f'attachment; filename="{filename}"'
+
             return response
+
         except Exception as e:
             return HttpResponse(f"Error: {str(e)}", content_type="text/plain", status=500)
