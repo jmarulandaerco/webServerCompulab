da223e8771a5c907f5eae8ff8ad655af951a4ad5
jmarulandaerco <josemarulanda63@hotmail.com>
2025-03-05 16:18:49 -0500

    Arreglando error de logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,41 +1,28 @@
-import configparser
-import os
 import logging
+from dataclasses import dataclass
+import os
 
-from utils.configfiles import ConfigFilePaths
 
+LOG_FILE = "log.log"
+MAX_LOG_SIZE = 5 * 1024 * 1024  # 5 MB
 
-config = configparser.ConfigParser(interpolation=None)
-cf = ConfigFilePaths()
-list_path_menu = cf.to_list()
+def check_log_size():
+    if os.path.exists(LOG_FILE) and os.path.getsize(LOG_FILE) > MAX_LOG_SIZE:
+        os.remove(LOG_FILE)  # Elimina el archivo si supera el tamaño máximo
 
-class CustomFileHandler(logging.FileHandler):
-    def __init__(self, filename, mode='a', maxBytes=500 * 1024 * 1024, encoding=None, delay=False):
-        self.maxBytes = maxBytes
-        super().__init__(filename, mode, encoding, delay)
-    
-    def emit(self, record):
-        try:
-            if os.path.exists(self.baseFilename) and os.path.getsize(self.baseFilename) >= self.maxBytes:
-                self.stream.close()
-                os.remove(self.baseFilename)
-                self.stream = self._open()
-        except Exception:
-            self.handleError(record)
-        super().emit(record)
-
+@dataclass
 class LoggerHandler:
     name: str = __name__
 
     def __post_init__(self):
+        check_log_size()
         self.logger = logging.getLogger(self.name)
         self.logger.setLevel(logging.INFO)
 
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.INFO)
-        
-        config.read(list_path_menu[0])        
-        file_handler = CustomFileHandler("logDjango.log", mode="a", maxBytes=int(config.get('DEFAULT', 'max_size_bytes')))
+
+        file_handler = logging.FileHandler("log.log", mode="a")
         file_handler.setLevel(logging.INFO)
 
         formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
