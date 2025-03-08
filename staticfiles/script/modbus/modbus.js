5333b46e9c938111ebd2feaff34ad070765f5867
jmarulandaerco <josemarulanda63@hotmail.com>
2025-03-07 22:46:43 -0500

    edit - master edi view Datatable

--- staticfiles/script/modbus/modbus.js
+++ staticfiles/script/modbus/modbus.js
@@ -20,6 +20,7 @@ function loadDevices(page) {
     const fullUrl = `/api/modbus/devices/?device=${encodeURIComponent(page)}`; 
     fetch(fullUrl)  
         .then(response => {
+            console.log(response);
             if (!response.ok) {
                 alert(`Error: ${response.statusText}`);
             }
@@ -405,7 +406,7 @@ async function addDeviceTcp() {
 
 async function deleteDevice(device) {
     try {
-
+        console.log(device)
         if (confirm("Are you sure to erase the device?")) {
             const response = await fetch(mapFolder, {
                 method: "DELETE",
