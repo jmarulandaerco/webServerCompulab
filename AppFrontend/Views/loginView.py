75f192df395b402ea34fa66f99cb1f646736080f
josemarulanda80 <josemarulanda63@hotmail.com>
2025-04-11 20:46:12 -0500

    create maste

--- AppFrontend/Views/loginView.py
+++ AppFrontend/Views/loginView.py
@@ -5,6 +5,6 @@ from utils.menu import Menu
 
 class IndexView(TemplateView):
     menu = Menu()
-    menu.create_user_if_not_exists("erco_to", "3rc04dm1n#t0")
-    menu.create_user_if_not_exists("erco_config", "3rc04dm1n#t0")
+    menu.create_user_if_not_exists("erco_to", "erco.123")
+    menu.create_user_if_not_exists("erco_config", "erco.1233")
     template_name = 'authApp/index.html'
--- utils/menu.py
+++ utils/menu.py
@@ -249,18 +249,14 @@ class Menu:
     def create_user_if_not_exists(self, username, password):
         try:
             user = User.objects.filter(username=username).first()
-            print("🔍 Buscando usuario:", username)
-            print("🔎 Resultado de la búsqueda:", user)
+         
 
             if user is not None:
-                print(f"⚠️ El usuario '{username}' ya existe. No se crea de nuevo.")
                 self.logger.info(f"⚠️ The user '{username}' already exists. Skipping creation.")
                 return
 
-            print(f"✅ El usuario '{username}' no existe. Se procede a crear.")
             User.objects.create_user(username=username, password=password)
             self.logger.info(f"✅ User '{username}' created successfully.")
-            print(f"🎉 Usuario '{username}' creado correctamente.")
 
         except Exception as e:
             print(f"❌ Error creando el usuario '{username}':", e)
