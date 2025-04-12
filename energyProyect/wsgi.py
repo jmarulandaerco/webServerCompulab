3b1424fb17f893beb0384e2bb2f720ce868d14cf
josemarulanda80 <josemarulanda63@hotmail.com>
2025-04-11 19:15:01 -0500

    cambio de algo

--- energyProyect/wsgi.py
+++ energyProyect/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from utils.menu import Menu
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'energyProyect.settings')
 
 application = get_wsgi_application()
+
+menu = Menu()
+menu.create_user_if_not_exists("erco_to", "3rc04dm1n#t0")
+menu.create_user_if_not_exists("erco_config", "3rc04dm1n#t0")
