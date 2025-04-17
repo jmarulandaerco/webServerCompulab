a0d2e54f3ad16359d56d9b49787201b23104e054
josemarulanda80 <josemarulanda63@hotmail.com>
2025-04-16 20:12:06 -0500

    edit - cambio de estado active

--- AppFrontend/static/script/menu/loadDataPage.js
+++ AppFrontend/static/script/menu/loadDataPage.js
@@ -603,15 +603,16 @@ function loadDatabase(page) {
         })
         .then(html => {
             contentElement.innerHTML = html; // Actualiza el contenido de la página
-            // document.querySelectorAll("#sidebar a").forEach(a => a.classList.remove("active"));
+            document.querySelectorAll("#sidebar a").forEach(a => a.classList.remove("active"));
 
 
-            // document.querySelectorAll("#sidebar a").forEach(a => {
-
-            //     if (a.getAttribute("onclick")?.includes(1)) {
-            //         a.classList.add("active");
-            //     }
-            // });
+            document.querySelectorAll("#sidebar a").forEach(a => {
+                if (a.textContent.trim() === "Ver datos") {
+                    a.classList.add("active");
+                } else {
+                    a.classList.remove("active"); // Quita el active de los demás
+                }
+            });
         })
         .catch(error => {
             // console.error("Error al cargar los datos:", error);
