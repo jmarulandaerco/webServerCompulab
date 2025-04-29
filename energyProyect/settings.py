16540b195f83ea9763c597c4f2ee63a4031ec244
josemarulanda80 <josemarulanda63@hotmail.com>
2025-04-28 20:01:23 -0500

    lenguage_spanis

--- energyProyect/settings.py
+++ energyProyect/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 ]
 
 SIMPLE_JWT = { 
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5), 
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1), 
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1), 
     'ROTATE_REFRESH_TOKENS': False, 
     'BLACKLIST_AFTER_ROTATION': False, 
