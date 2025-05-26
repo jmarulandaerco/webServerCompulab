4da705152bd9fdd1cbede0dc612327a8d4e49b10
josemarulanda80 <josemarulanda63@hotmail.com>
2025-05-26 13:36:35 -0500

    Se arreglo funcion para quitar configuración de momento

--- AppFrontend/static/script/menu/settingSystem.js
+++ AppFrontend/static/script/menu/settingSystem.js
@@ -512,6 +512,8 @@ async function addPLC(){
             setTimeout(() => {
                 deletePLC(interface,ip,port);
                 console.log("Se logro")
+                console.log(port)
+
             }, delay);
 
 
